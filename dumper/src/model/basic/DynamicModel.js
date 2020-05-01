1d03988162f2e9a754d935cdc9a98fdc551d87f7
daniel097541 <32720562+daniel097541@users.noreply.github.com>
2020-05-01 13:17:56 +0200

    Now filtering null and empty values

--- dumper/src/model/basic/DynamicModel.js
+++ dumper/src/model/basic/DynamicModel.js
@@ -1,8 +1,14 @@
 class DynamicModel {
     constructor(fieldNames, fieldValues) {
-        for(let i = 0; i < fieldNames.length; i++){
+        for (let i = 0; i < fieldNames.length; i++) {
             const name = fieldNames[i];
-            this[name] = fieldValues[i];
+            const value = fieldValues[i];
+            if (value && value !== '-' && value !== '') {
+                this[name] = value;
+            }
+            else{
+                this[name] = null;
+            }
         }
     }
 }
