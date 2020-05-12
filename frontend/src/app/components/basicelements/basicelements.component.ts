8cfcd915cd9db620daa953d623753594143011ba
Juan Manuel <jmveranogarri@hotmail.com>
2020-05-12 19:03:43 +0200

    añadido consulta por nombre

--- frontend/src/app/components/basicelements/basicelements.component.ts
+++ frontend/src/app/components/basicelements/basicelements.component.ts
@@ -1,59 +1,80 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ColumnMode } from '@swimlane/ngx-datatable';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ColumnMode} from '@swimlane/ngx-datatable';
 import {CharacterService} from '../../services/character.service';
 import {Character} from '../../models/Character';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort, Sort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
 
 
 @Component({
-  selector: 'app-basicelements',
-  templateUrl: './basicelements.component.html',
-  styleUrls: ['./basicelements.component.scss']
+    selector: 'app-basicelements',
+    templateUrl: './basicelements.component.html',
+    styleUrls: ['./basicelements.component.scss']
 })
 export class BasicelementsComponent implements OnInit {
     simpleSlider = 40;
     doubleSlider = [20, 60];
     state_default: boolean = true;
     focus: any;
-    data : Array<any>=[];
-    rows : Array<Character>=[];
+    data: Array<any> = [];
+    rows: Array<Character> = [];
     ColumnMode = ColumnMode;
     public dataSourceFront: MatTableDataSource<Character>;
+    nombreCharacter:string;
 
     @ViewChild(MatPaginator) paginator: MatPaginator;
     @ViewChild(MatSort) sort: MatSort;
 
     // Columnas de las Tablas LVs
-    public displayedColumns: string[]=['nombre'];
-    constructor( private _characterService:CharacterService) {
+    public displayedColumns: string[] = ['nombre'];
+
+    constructor(private _characterService: CharacterService) {
         this.dataSourceFront = new MatTableDataSource();
-
-
-   }
-
-    ngOnInit() {
-        this._characterService.getCharacters().subscribe(data=>{
-            this.data=data;
-            this.data.forEach(c=>{
-                this.rows.push(new Character(c.name))
-            })
-            this.dataSourceFront.data = this.rows;
-            this.dataSourceFront.sort = this.sort;
-            this.dataSourceFront.paginator = this.paginator;
-        })
-
-
-
-
 
 
     }
 
+    ngOnInit() {
+    this.mostararCharactersByName();
+
+    }
 
 
+    mostararCharacters() {
+        this._characterService.getCharacters().subscribe(data => {
+            this.data = data;
+            this.data.forEach(c => {
+                this.rows.push(new Character(c.name))
+            })
+            this.dataSourceFront.sort = this.sort;
 
+            const sortState: Sort = {active: 'nombre', direction: 'asc'};
+            this.sort.active = sortState.active;
+            this.sort.direction = sortState.direction;
+            this.sort.sortChange.emit(sortState);
+            this.dataSourceFront.data = this.rows;
+            this.dataSourceFront.paginator = this.paginator;
+        })
+    }
+
+    mostararCharactersByName() {
+        this._characterService.getCharactersByNombre(this.nombreCharacter).subscribe(data => {
+            console.log(data)
+            this.data = data;
+            this.data.forEach(c => {
+                this.rows.push(new Character(c.name))
+            })
+            this.dataSourceFront.sort = this.sort;
+
+            const sortState: Sort = {active: 'nombre', direction: 'asc'};
+            this.sort.active = sortState.active;
+            this.sort.direction = sortState.direction;
+            this.sort.sortChange.emit(sortState);
+            this.dataSourceFront.data = this.rows;
+            this.dataSourceFront.paginator = this.paginator;
+        })
+    }
 
 
 }
