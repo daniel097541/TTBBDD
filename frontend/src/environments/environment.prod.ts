a1779d587be69dfba38f3d115a38f4b8ac486d38
daniel097541 <32720562+daniel097541@users.noreply.github.com>
2020-05-21 00:34:26 +0200

    Removed slash

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'http://ec2-15-236-90-35.eu-west-3.compute.amazonaws.com:3000/'
+  API_URL: 'http://ec2-15-236-90-35.eu-west-3.compute.amazonaws.com:3000'
 };
